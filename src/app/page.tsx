80c732dbffad0fcef532727098a8c29ea13dacc3
neerajkumarc <chaudharyneeraj91@gmail.com>
2025-02-16 15:57:50 +0530

    install sidebar component from shadcn & add a side-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Home() {
   return (
-  <div>tender task board</div>
+    <SidebarProvider>
+      <AppSidebar />
+    </SidebarProvider>
   );
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import * as React from "react";
+import { Phone, Search, SquareKanban, TrendingUp } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Kanban",
+    url: "#",
+    icon: SquareKanban,
+  },
+  {
+    title: "Trending",
+    url: "#",
+    icon: TrendingUp,
+  },
+  {
+    title: "Phone",
+    url: "#",
+    icon: Phone,
+  },
+];
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props} className="bg-black">
+      <SidebarHeader className="flex justify-end items-end">
+        <SidebarTrigger />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarMenu className="py-8 px-2">
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <Link href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
